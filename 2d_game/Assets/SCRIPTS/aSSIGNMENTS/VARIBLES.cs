c06c21181796f8a8b23051520cb5d7dc81a835c4
jake <jakeis4narnia@gmail.com>
2019-08-26 17:19:30 -0600

    new

---
+++ 2d_game/Assets/SCRIPTS/aSSIGNMENTS/VARIBLES.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VARIBLES : MonoBehaviour
+{
+    string names = "Jake";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(names);
+    
+    }
+
+    // Update is called once per frame
+    
+    
+        
+    
+    
+}
