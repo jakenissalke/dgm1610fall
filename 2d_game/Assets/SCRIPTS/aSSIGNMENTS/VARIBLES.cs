ebd5ec5a375fa3004f53785f748b9ae2f497185f
jake <jakeis4narnia@gmail.com>
2019-08-28 17:04:57 -0600

    i dont know whats going on

--- 2d_game/Assets/SCRIPTS/aSSIGNMENTS/VARIBLES.cs
+++ 2d_game/Assets/SCRIPTS/aSSIGNMENTS/VARIBLES.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class VARIBLES : MonoBehaviour
 {
     string names = "Jake";
-
-
-    // Start is called before the first frame update
+    int age = 18;
+    float height = 6.1f;
+    bool inrelationship = true;
     void Start()
     {
-        Debug.Log(names);
+    
+    Debug.Log(names);
+    Debug.Log(age);
+    Debug.Log(height); 
+    Debug.Log(inrelationship);
+   
+        
     
     }
 
-    // Update is called once per frame
     
     
-        
     
     
 }
