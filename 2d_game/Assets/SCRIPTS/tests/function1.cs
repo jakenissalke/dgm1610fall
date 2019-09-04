a8b6d0a302b0a6d66730e3912cd980c601fb6077
jake <jakeis4narnia@gmail.com>
2019-09-04 17:57:39 -0600

    classwork

---
+++ 2d_game/Assets/SCRIPTS/tests/function1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class function1 : MonoBehaviour
+{
+    int num1;
+    int num2;
+    int total;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CalcAdd(11,23);
+    }
+
+    int CalcAdd(int n1, int n2)
+    {
+        Debug.Log("Number 1 =" + n1 + "Number 2= " + n2);
+        total =  n1+ n2;
+
+        return total;
+        Debug.Log(total);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
