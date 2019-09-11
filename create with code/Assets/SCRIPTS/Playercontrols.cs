2eed7bae7d72932adb442c27cbc51a906035dea0
jake <jakeis4narnia@gmail.com>
2019-09-11 17:03:31 -0600

    forward

---
+++ create with code/Assets/SCRIPTS/Playercontrols.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontrols : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // this code makes the tanks move 
+        transform.Translate(Vector3.forward*Time.deltaTime*20);
+    }
+}
